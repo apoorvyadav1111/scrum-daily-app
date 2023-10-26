db2861645b046a3b1cb3bbbb1b6fc1cc3dcf1a96
ayadav16 <ayadav7@binghamton.edu>
2023-10-26 16:38:17 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Apoorv Yadav on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
